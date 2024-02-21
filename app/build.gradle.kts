2652c80bba9e39af4f29896257bfb43291fce112
yudi karma <yudikarmateknik@gmail.com>
2024-02-21 09:07:16 +0700

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,113 @@
+import app.web.drjackycv.buildsrc.Depends
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    id("kotlin-parcelize")
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    compileSdk = Depends.Versions.androidCompileSdkVersion
+
+    defaultConfig {
+        multiDexEnabled = true
+        applicationId = "app.web.drjackycv.mvvmtemplate"
+        minSdk = Depends.Versions.minSdkVersion
+        targetSdk = Depends.Versions.targetSdkVersion
+        versionCode = Depends.Versions.appVersionCode
+        versionName = Depends.generateVersionName()
+        testInstrumentationRunner =
+            Depends.Libraries.testInstrumentationRunner
+        javaCompileOptions.annotationProcessorOptions.arguments += mapOf(
+            "room.schemaLocation" to "$projectDir/schemas"
+        )
+    }
+    sourceSets {
+        map { it.java.srcDir("src/${it.name}/kotlin") }
+    }
+    buildTypes {
+        named("debug") {
+            isMinifyEnabled = false
+            isShrinkResources = false
+            isDebuggable = true
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-DEBUG"
+            signingConfig = signingConfigs.getByName("debug")
+        }
+        named("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            setProguardFiles(
+                listOf(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+                )
+            )
+        }
+    }
+    compileOptions {
+        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+    lintOptions {
+        isAbortOnError = false
+    }
+    //testOptions.unitTests.returnDefaultValues = true
+    packagingOptions {
+        exclude("META-INF/rxjava.properties")
+        exclude("META-INF/proguard/androidx-annotations.pro")
+        resources.excludes += "DebugProbesKt.bin"
+    }
+    namespace = "app.web.drjackycv.mvvmtemplate"
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+        freeCompilerArgs = freeCompilerArgs + listOf("-XXLanguage:+InlineClasses")
+    }
+}
+
+dependencies {
+    implementation(Depends.Libraries.kotlin)
+    implementation(Depends.Libraries.android_core_ktx)
+    implementation(Depends.Libraries.multidex)
+    implementation(Depends.Libraries.fragment_ktx)
+    implementation(Depends.Libraries.paging_runtime_ktx)
+    implementation(Depends.Libraries.paging_rx)
+    implementation(Depends.Libraries.dataStore_preferences)
+    implementation(Depends.Libraries.coroutines_android)
+    //dependency injection
+    implementation(Depends.Libraries.dagger)
+    kapt(Depends.Libraries.dagger_compiler)
+    implementation(Depends.Libraries.dagger_hilt_android)
+    implementation(Depends.Libraries.dagger_hilt_navigation_compose)
+    kapt(Depends.Libraries.dagger_hilt_compiler)
+//    kapt(Depends.Libraries.dagger_hilt_android_compiler)
+//    kapt(Depends.Libraries.hilt_androidx_compiler)
+    implementation(Depends.Libraries.java_inject)
+    //network
+    implementation(Depends.Libraries.retrofit)
+    implementation(Depends.Libraries.retrofit_adapter_rx)
+    implementation(Depends.Libraries.logging_interceptor)
+    //other
+    implementation(Depends.Libraries.timber)
+    implementation(Depends.Libraries.material)
+    debugImplementation(Depends.Libraries.leak_canary)
+    debugImplementation(Depends.Libraries.chucker)
+    releaseImplementation(Depends.Libraries.chucker_no_op)
+    //test
+    testImplementation(Depends.Libraries.junit)
+    androidTestImplementation(Depends.Libraries.test_runner)
+    androidTestImplementation(Depends.Libraries.espresso_core)
+
+    implementation(project(":presentation"))
+    implementation(project(":data"))
+    implementation(project(":domain"))
+}
---
+++ domain/build.gradle.kts
@@ -0,0 +1,80 @@
+import app.web.drjackycv.buildsrc.Depends
+
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    id("kotlin-parcelize")
+    kotlin("kapt")
+}
+
+android {
+
+    compileSdk = Depends.Versions.androidCompileSdkVersion
+
+    defaultConfig {
+        multiDexEnabled = true
+        minSdk = Depends.Versions.minSdkVersion
+        targetSdk = Depends.Versions.targetSdkVersion
+        testInstrumentationRunner =
+            Depends.Libraries.testInstrumentationRunner
+        consumerProguardFiles("consumer-rules.pro")
+    }
+    compileOptions {
+        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+        freeCompilerArgs = freeCompilerArgs + listOf("-XXLanguage:+InlineClasses")
+    }
+    sourceSets {
+        map { it.java.srcDir("src/${it.name}/kotlin") }
+    }
+    buildTypes {
+        named("debug") { }
+        named("release") {
+            isMinifyEnabled = true
+            setProguardFiles(
+                listOf(
+                    getDefaultProguardFile("proguard-android-optimize.txt"),
+                    "proguard-rules.pro"
+                )
+            )
+        }
+    }
+    namespace = "app.web.drjackycv.domain"
+
+}
+
+val unitTestImplementation: Configuration by configurations.creating
+configurations["compileOnly"].extendsFrom(unitTestImplementation)
+configurations["testImplementation"].extendsFrom(unitTestImplementation)
+
+dependencies {
+    implementation(Depends.Libraries.kotlin)
+    //android
+    implementation(Depends.Libraries.paging_runtime_ktx)
+    implementation(Depends.Libraries.paging_rx)
+    implementation(Depends.Libraries.lifecycle_livedata)
+    implementation(Depends.Libraries.multidex)
+    //reactive
+    implementation(Depends.Libraries.rx_kotlin)
+    implementation(Depends.Libraries.rx_java)
+    //dependency injection
+    implementation(Depends.Libraries.dagger)
+    kapt(Depends.Libraries.dagger_compiler)
+    implementation(Depends.Libraries.dagger_hilt_android)
+//    implementation(Depends.Libraries.dagger_hilt_core)
+    kapt(Depends.Libraries.dagger_hilt_compiler)
+//    kapt(Depends.Libraries.dagger_hilt_android_compiler)
+//    kapt(Depends.Libraries.hilt_androidx_compiler)
+    implementation(Depends.Libraries.java_inject)
+    //test
+    testImplementation(Depends.Libraries.junit)
+    testImplementation(Depends.Libraries.mockito_core)
+    testImplementation(Depends.Libraries.mockito_inline)
+    testImplementation(Depends.Libraries.coroutines_test)
+    testImplementation(Depends.Libraries.mockito_kotlin)
+    testImplementation(Depends.Libraries.mockk)
+    testImplementation(Depends.Libraries.arch_core_testing)
+}
